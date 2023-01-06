073dddb85b7f2bf680c065d06e981b5ca5fafb82
luweglarz <lucasweglarz9@gmail.com>
2023-01-06 12:52:00 +0100

    need to do sorting with en_US.UTF-8

--- Dockerfile
+++ Dockerfile
@@ -5,6 +5,11 @@ RUN apt-get update \
 && apt-get install -y locales \
 && apt -y install valgrind 
 
+#Build locales to have correct alpha sorting
+RUN sed -i '/en_US.UTF-8/s/^# //g' /etc/locale.gen && \
+    locale-gen
+ENV LC_ALL en_US.UTF-8 
+
 RUN mkdir ft_ls
 
 CMD tail -f /dev/null
--- srcs/t_file/file_compare.c
+++ srcs/t_file/file_compare.c
@@ -1,5 +1,12 @@
 #include "../../includes/ft_ls.h"
 
+
+int get_alpha(char c){
+    if (c >= 'A' && c <= 'Z')
+        return (c + 32);
+    return (c);
+}
+
 bool alphabetic_compare(char *s1, char *s2){
     int i = 0;
     int j = 0;
@@ -8,13 +15,17 @@ bool alphabetic_compare(char *s1, char *s2){
         return (false);
     else if ((s1[0] == '.' && s1[1] == '.' && s1[2] == '\0') && (s2[0] == '.' && s2[1] == '\0'))
         return (true);
-    while(s1[i] && s2[j]){
-        if (s1[i] != s2[j])
+    while(get_alpha(s1[i]) && get_alpha(s2[j])){
+        while(!ft_isalnum(s1[i]))
+            i++;
+        while(!ft_isalnum(s2[j]))
+            j++;
+        if (get_alpha(s1[i]) != get_alpha(s2[j]))
             break ;
         i++;
         j++;
     }
-    return (s1[i] > s2[j]);
+    return (get_alpha(s1[i]) > get_alpha(s2[j]));
 }
 
 bool time_compare(t_file *file1, t_file *file2){
